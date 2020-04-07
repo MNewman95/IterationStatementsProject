0025d0cba53599f272dda54cfb7c149c160be380
Michael Newman <michaelnewman95@gmail.com>
2020-04-07 10:45:25 -0500

    finished

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -10,32 +10,49 @@ namespace IterationStatements
             //TODO
 
             //Create a List called "numbers"
+            var numbers = new List<int>();
             //Create a variable of type int with an initializer of 0
-
-
+            var init = 0;
 
             // Create a do-while loop
+            do
             {
                 // Increment your variable by 1
+                init++;
                 // Then add your variable to "numbers"
+                numbers.Add(init);
+
             }
             // While your variable is less than 100
-
+            while (init < 100);
 
 
             // Create a while loop
             // While your variable is less than 200
+            while (init < 200)
             {
                 // Increment your variable by 1
+                init++;
                 // Then add your variable to "numbers"
+                numbers.Add(init);
             }
 
             Console.WriteLine("Increase:");
 
             // Create a foreach loop
-            // Write your variable to the console
 
-            Console.WriteLine("");
+
+           
+
+            foreach (var num in numbers)
+
+
+            // Write your variable to the console
+            {
+                Console.WriteLine(num);
+            }
+
+                Console.WriteLine("");
             Console.WriteLine("Decrease:");
 
             // Create a for loop
@@ -43,8 +60,10 @@ namespace IterationStatements
             // in your conditional, as long as i is less than or equal to the length of "numbers"
             // and as long as i is greater than or equal to 0
             // Decrement i by 1
+            for (var i = 199; i <= numbers.Count && i >= 0; i--)
             {
                 // Write to the console "numbers" at index i
+                Console.WriteLine(numbers[i]);
             }
         }
     }
